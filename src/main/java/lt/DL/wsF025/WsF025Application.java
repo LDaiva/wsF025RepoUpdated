39500bd025c39130acd5facb1ccdb31081c92da6
@LDaiva <daivales@yahoo.co.uk>
2024-04-04 14:44:23 +0300

    Initial commit

---
+++ src/main/java/lt/DL/wsF025/WsF025Application.java
@@ -0,0 +1,13 @@
+package lt.DL.wsF025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsF025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsF025Application.class, args);
+	}
+
+}
---
+++ src/test/java/lt/DL/wsF025/WsF025ApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.DL.wsF025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsF025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
